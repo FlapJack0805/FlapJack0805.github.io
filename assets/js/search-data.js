888c70d48aa266775a95f9b51b155045269b3495
FlapJack0805 <153259125+FlapJack0805@users.noreply.github.com>
2025-01-08 14:50:03 +0000

    Deploying to gh-pages from @ FlapJack0805/FlapJack0805.github.io@4bac7fde76dba98356b542a5fd37e26559e17928 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,51 +442,36 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-solar-car-light-board",
+          section: "News",},{id: "projects-a-algorithm",
+          title: 'A* Algorithm',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/astar/";
+            },},{id: "projects-euchre",
+          title: 'Euchre',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/euchre/";
+            },},{id: "projects-four-function-calculator",
+          title: 'Four Function Calculator',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/four_func_calc/";
+            },},{id: "projects-image-resizer",
+          title: 'Image Resizer',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/image_resizer/";
+            },},{id: "projects-solar-car-light-board",
           title: 'Solar Car Light Board',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+              window.location.href = "/projects/light_board/";
+            },},{id: "projects-pyranometer-board",
+          title: 'Pyranometer Board',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/pyranometer_board/";
             },},{
         id: 'social-email',
         title: 'email',
